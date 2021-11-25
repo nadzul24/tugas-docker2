bd86f1a8a22785b7c858375ab9fc2a5addc091b2
nadzul24 <nadnadzul@gmail.com>
2021-11-25 21:51:29 +0700

    first commit

---
+++ php/src/index.php
@@ -0,0 +1,35 @@
+<?php
+echo "Hello there, this is a PHP Apache container";
+
+// The MySQL service named in the docker-compose.yml.
+$host = '172.20.0.2';
+
+// Database use name
+$user = 'root';
+
+//database user password
+$pass = 'admin';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+
+// select query
+$sql = 'SELECT * FROM users';
+
+if ($result = $conn->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->username . " " . $user->password;
+    echo "<br>";
+}
+?>
